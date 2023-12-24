82bb40b43ecac0f8e2e8dc70a68403c0de5e4ec7
Binh <lechibinh0511@gmail.com>
2023-12-24 22:28:29 +0700

    first commit

---
+++ SoLuotTruyCap.sql
@@ -0,0 +1,88 @@
+﻿Create proc sp_ThongKe
+as
+BEGIN
+	declare @SoTruyCapGanNhat int
+	declare @Count int
+	select @Count = count(*)
+	from ThongKes
+	if @Count is null set @Count = 0
+	if @Count = 0
+		insert into ThongKes(ThoiGian, SoTruyCap)
+		values(GETDATE(), 1)
+	else
+		begin
+			declare @ThoiGianGanNhat datetime
+			select @SoTruyCapGanNhat = tk.SoTruyCap, @ThoiGianGanNhat = tk.ThoiGian
+			from ThongKes tk
+			where tk.ID = (select max(tk1.ID) from ThongKes tk1)
+			if @SoTruyCapGanNhat is null  set @SoTruyCapGanNhat = 0
+			if @ThoiGianGanNhat is null set @ThoiGianGanNhat = GETDATE()
+			--Nếu chuyển sang ngày mới chưa (sau 12 giờ đêm)
+			--Nếu chưa chuyển sang ngày mới thì update
+			if day(@ThoiGianGanNhat) = day(getdate())
+				begin
+					update ThongKes
+					set SoTruyCap = @SoTruyCapGanNhat + 1, ThoiGian = GETDATE()
+					where ID = (select max(tk1.ID) from ThongKes tk1)
+				end
+				--Nếu đã sang ngày mới thì insert thêm 1 bản ghi mới
+				else
+					insert into ThongKes(ThoiGian, SoTruyCap)
+					values(GETDATE(), 1)
+		end
+		--Số truy cập ngày hôm nay
+		declare @HomNay int
+		set @HomNay = DATEPART(DW,GETDATE())
+		select @SoTruyCapGanNhat = SoTruyCap, @ThoiGianGanNhat = ThoiGian
+		from ThongKes
+		where ID=(select max(ID) from ThongKes)
+		--Số truy cập ngày hôm qua
+		declare @TruyCapHomQua bigint	
+		select @TruyCapHomQua = ISNULL(SoTruyCap, 0)
+		from ThongKes
+		where CONVERT(nvarchar(20), ThoiGian, 103) = CONVERT(nvarchar(20), GETDATE()-1, 103)
+		if @TruyCapHomQua is null set @TruyCapHomQua = 0
+
+		--Truy cập đầu tuần này
+		DECLARE @DauTuanNay datetime
+		SET @DauTuanNay= DATEADD(wk, DATEDIFF(wk, 6, GetDate()), 6)
+		-- Tính Ngày đầu của tuần trước Tính từ thời điểm 00:00:))
+		DECLARE @NgayDauTuanTruoc datetime
+		SET @NgayDauTuanTruoc = Cast(CONVERT(nvarchar(30),DATEADD(dd, -(@HomNay+6), GetDate()),101) AS datetime)
+		-- Tính ngày cuối tuần trước tính đến 24h ngày cuối tuần 
+		DECLARE @NgayCuoiTuanTruoc datetime
+		SET @NgayCuoiTuanTruoc = Cast(CONVERT(nvarchar(30),DATEADD(dd, -@HomNay, GetDate()),101) +' 23:59:59' AS datetime)
+
+		--Số truy cập tuần này
+		declare @TruyCapTuanNay bigint
+		select @TruyCapTuanNay = ISNULL(sum(SoTruyCap), 0)
+		from ThongKes
+		where ThoiGian between @DauTuanNay and GETDATE()	
+		--Số truy cập tuần trước
+		declare @SoLanTruyCapTuanTruoc bigint
+		select @SoLanTruyCapTuanTruoc = ISNULL(sum(SoTruyCap), 0)
+		from ThongKes ttk
+		where ttk.ThoiGian between @NgayDauTuanTruoc and @NgayCuoiTuanTruoc
+		--Số truy cập tháng này
+		declare @SoTruyCapThangNay bigint 
+		select @SoTruyCapThangNay=isnull(sum(SoTruyCap),0)
+		from ThongKes ttk 
+		where MONTH(ttk.ThoiGian)=MONTH(GETDATE())
+		--Số truy cập tháng trước
+		declare @SoTruyCapThangTruoc bigint 
+		select @SoTruyCapThangTruoc=isnull(sum(SoTruyCap),0)
+		from ThongKes ttk 
+		where MONTH(ttk.ThoiGian)=MONTH(GETDATE())-1
+
+		-- Tính tổng số
+		DECLARE @TongSo bigint
+		SELECT  @TongSo=isnull(Sum(SoTruyCap),0) FROM ThongKes ttk
+		
+		SELECT @SoTruyCapGanNhat AS HomNay, 
+		@TruyCapHomQua AS HomQua,
+		@TruyCapTuanNay AS TuanNay, 
+		@SoLanTruyCapTuanTruoc AS TuanTruoc, 
+		@SoTruyCapThangNay AS ThangNay, 
+		@SoTruyCapThangTruoc AS ThangTruoc,
+		@TongSo AS TatCa
+END
---
+++ WebBanHangOnline/Controllers/WishlistController.cs
@@ -0,0 +1,75 @@
+﻿using PagedList;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Services.Description;
+using System.Web.UI;
+using WebBanHangOnline.Models;
+using WebBanHangOnline.Models.EF;
+
+namespace WebBanHangOnline.Controllers
+{
+ 
+    public class WishlistController : Controller
+    {
+        // GET: Wishlist
+        public ActionResult Index(int? page)
+        {
+            var pageSize = 1;
+            if (page == null)
+            {
+                page = 1;
+            }
+            IEnumerable<Wishlist> items = db.Wishlists.Where(x => x.UserName == User.Identity.Name).OrderByDescending(x => x.CreatedDate);
+            var pageIndex = page.HasValue ? Convert.ToInt32(page) : 1;
+            items = items.ToPagedList(pageIndex, pageSize);
+            ViewBag.PageSize = pageSize;
+            ViewBag.Page = page;
+            return View(items);
+        }
+
+        [HttpPost]
+        [AllowAnonymous]
+        public ActionResult PostWishList(int ProductID)
+        {
+            if (Request.IsAuthenticated == false)
+            {
+                return Json(new { Success = false, Message = "Bạn chưa đăng nhập..." });
+            }
+            var checkItem = db.Wishlists.FirstOrDefault(x=>x.ProductID == ProductID &&  x.UserName == User.Identity.Name);
+            if (checkItem != null)
+            {
+                return Json(new { Success = false, Message = "Sản phẩm đã được thêm vào danh sách yêu thích rồi" });
+            }
+            var item = new Wishlist();
+            item.ProductID = ProductID;
+            item.UserName = User.Identity.Name;
+            item.CreatedDate = DateTime.Now;
+            db.Wishlists.Add(item);
+            db.SaveChanges();
+            return Json(new {Success = true});
+        }
+
+        [HttpPost]
+        [AllowAnonymous]
+        public ActionResult PostDeleteWishlist(int ProductID)
+        {
+            var checkItem = db.Wishlists.FirstOrDefault(x => x.ProductID == ProductID && x.UserName == User.Identity.Name);
+            if (checkItem != null)
+            {
+                db.Wishlists.Remove(checkItem);
+                db.SaveChanges();
+                return Json(new { Success = true, Message = "Xóa thành công" });
+            }
+            return Json(new { Success = false, Message = "Xóa thất bại" });
+        }
+
+        private ApplicationDbContext db = new ApplicationDbContext();
+        protected override void Dispose(bool disposing)
+        {
+            base.Dispose(disposing);
+        }
+    }
+}
